32d77fca7879490d065d35233aebebfc2fe036e7
Dhanielr <dhanielr@hotmail.com>
2020-12-27 12:14:54 -0300

    updating(InProgress): Update structure for chakra ui v1.

--- src/contexts/theme/ThemeContainer.tsx
+++ src/contexts/theme/ThemeContainer.tsx
@@ -1,15 +1,18 @@
 import { ColorModeProvider, CSSReset, ThemeProvider as ChakraThemeProvider } from '@chakra-ui/core';
-import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
 import React from 'react';
 import theme from 'styles/theme';
 
 const ThemeContainer: React.FC = ({ children }) => (
   <ChakraThemeProvider theme={theme}>
-    <ColorModeProvider value="dark">
-      <EmotionThemeProvider theme={theme}>
-        <CSSReset />
-        {children}
-      </EmotionThemeProvider>
+    <ColorModeProvider
+      options={{
+        initialColorMode: 'dark',
+        useSystemColorMode: false,
+      }}
+      value="dark"
+    >
+      <CSSReset />
+      {children}
     </ColorModeProvider>
   </ChakraThemeProvider>
 );
